fe49ce3d22adb31d49ac58cd0fb7f0f6c3d01a6e
Leandro Ubilla <leandroubilla@protonmail.com>
2024-10-21 17:00:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+const App: React.FC = () => {
+  return (
+    <div className="h-screen bg-base-300 relative">
+      <div className="mx-auto max-w-2xl pt-80">
+        <h1 className="text-xl font-semibold mb-4">
+          React Notifications
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+
+import './index.css' // Import your CSS file here
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
